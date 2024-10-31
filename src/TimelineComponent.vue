ef80f48830f49907e725dcede5200e3f0ced21c8
poisnada <horberlan@gmail.com>
2024-10-31 20:19:19 -0300

    chore: remove hasCustomContent props conditional rendering

--- src/TimelineComponent.vue
+++ src/TimelineComponent.vue
@@ -16,14 +16,7 @@
         }"
       ></div>
       <div class="timeline-content">
-        <template v-if="!hasCustomContent">
-          <h4 class="event-title">{{ event.title }}</h4>
-          <p class="event-date">{{ event.date }}</p>
-          <p class="event-description">{{ event.description }}</p>
-        </template>
-        <template v-else>
-          <slot :event="event" :index="index" />
-        </template>
+        <slot :event="event" :index="index" />
       </div>
     </div>
     <div ref="timelineLine" class="timeline-line"></div>
@@ -140,6 +133,7 @@ export default defineComponent({
         timelineLine.value.style.height = props.lineWidth;
       }
     };
+
     onMounted(() => {
       getLayoutAndSetDirection();
     });
@@ -185,12 +179,14 @@ export default defineComponent({
   margin-inline: 2rem;
 }
 
-.event-title {
-  margin-top: 0;
+.timeline-event.horizontal-layout {
+  display: flex;
+  align-items: flex-start;
+  margin-block: 2rem;
 }
 
-.event-date {
-  font-size: 0.9rem;
+.timeline-event.horizontal-layout.timeline-marker {
+  margin-inline-end: 1rem;
 }
 
 .timeline-line {

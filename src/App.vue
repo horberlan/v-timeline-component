0ff04e34f6b2d58e9547b66a5bc9f2095f38ffad
poisnada <horberlan@gmail.com>
2024-10-31 20:23:15 -0300

    chore: change timeline-component sample

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,78 @@
 <template>
-  <!-- <TimelineComponent has-custom-content="false" /> -->
   <TimelineComponent
-    :has-custom-content="true"
-    color="#ddd"
-    merker-size="0.75rem"
+    color="purple"
+    marker-size="0.75rem"
+    layout="vertical"
     :timeline-events="timelineEvents"
-    layout="horizontal"
-    >
-    <template v-slot="{ event, index }">
-      <h4>{{ event.title }}</h4>
-      <p>Event Index: {{ index }}</p>
+  >
+    <template v-slot="{ event }: { event: TimelineEvent }" #default>
+      <p>{{ event.title }}</p>
+      <p>{{ event.description }}</p>
     </template>
   </TimelineComponent>
 </template>
 
-<script setup>
-import { ref } from "vue";
+<script setup lang="ts">
+import { ref, type Ref } from "vue";
 import TimelineComponent from "./TimelineComponent.vue";
+import vTimelineComponentSlot from "./v-timeline-component-slot.vue";
 
-const timelineEvents = ref([
+interface TimelineEvent {
+  title: string;
+  date: string;
+  description: string;
+  child?: TimelineEvent[];
+}
+
+const timelineEvents: Ref<TimelineEvent[]> = ref([
   {
-    title: "Event 1 Title",
+    title: "Lorem ipsum dolor sit amet",
     date: "2023-02-15",
-    description: "Description for the first event. This can be any text.",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet nulla auctor, vestibulum magna sed, convallis ex.",
+    child: [
+      {
+        title: "Lorem ipsum dolor sit amet 1",
+        date: "2023-02-16",
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet nulla auctor, vestibulum magna sed, convallis ex.",
+      },
+      {
+        title: "Lorem ipsum dolor sit amet 2",
+        date: "2023-02-17",
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet nulla auctor, vestibulum magna sed, convallis ex.",
+      },
+      {
+        title: "Lorem ipsum dolor sit amet 2",
+        date: "2023-02-17",
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet nulla auctor, vestibulum magna sed, convallis ex.",
+      },
+    ],
   },
   {
-    title: "Event 2 Title",
+    title: "Lorem ipsum dolor sit amet 2",
     date: "2023-03-01",
-    description: "This is the second event on our timeline.",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
   },
   {
-    title: "Event 3 Title",
+    title: "Lorem ipsum dolor sit amet 3",
     date: "2023-04-20",
     description:
-      "Aperiam animi ut. Odit ullam eaque. Iusto laboriosam non nulla nisi soluta nobis est dolor ea",
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet nulla auctor, vestibulum magna sed, convallis ex, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
   },
   {
-    title: "Event 4 Title",
+    title: "Lorem ipsum dolor sit amet 4",
     date: "2023-01-20",
     description:
-      "Aperiam animi ut. Odit ullam eaque. Iusto laboriosam non nulla nisi soluta nobis est dolor ea.",
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet nulla auctor, vestibulum magna sed, convallis ex, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
   },
 ]);
 </script>
 
-<style scoped></style>
+<style scoped>
+* p {
+  margin: 0;
+}
+</style>
